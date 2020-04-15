69c20a916a0a6abba7f5bbb778529c62fd02416b
VigoWong <z5247672@ad.unsw.edu.au>
2020-04-15 16:31:47 +1000

    initialization

---
+++ 9024/assign_1/ass2-getting-started/cmake-build-debug/CMakeFiles/ass2_getting_started.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass2_getting_started.dir/exTkns.c.o"
+  "ass2_getting_started"
+  "ass2_getting_started.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ass2_getting_started.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
